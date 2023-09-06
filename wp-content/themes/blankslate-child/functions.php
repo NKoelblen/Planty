9d91de5705a2767d8da29eac8b05838e4bafd2ce
Noémie Koelblen <noemie.koelblen@icloud.com>
2023-09-06 12:02:41 +0200

    Page template, color palette

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -15,5 +15,39 @@ add_action('after_setup_theme', 'planty_setup');
 function planty_setup()
 {
     add_theme_support('custom-logo');
+
     register_nav_menus(array('footer-menu' => esc_html__('Menu Pied de page')));
+
+    add_theme_support('editor-color-palette', array(
+        array(
+            'name'  => esc_attr__('Noir', 'themeLangDomain'),
+            'slug'  => 'noir',
+            'color' => '#000000',
+        ),
+        array(
+            'name'  => esc_attr__('Blanc', 'themeLangDomain'),
+            'slug'  => 'blanc',
+            'color' => '#FFFFFF',
+        ),
+        array(
+            'name'  => esc_attr__('Beige', 'themeLangDomain'),
+            'slug'  => 'beige',
+            'color' => '#ECE2DA',
+        ),
+        array(
+            'name'  => esc_attr__('Rose', 'themeLangDomain'),
+            'slug'  => 'rose',
+            'color' => '#D2776A',
+        ),
+        array(
+            'name'  => esc_attr__('Rose soutenu', 'themeLangDomain'),
+            'slug'  => 'rose-soutenu',
+            'color' => '#C02E44',
+        ),
+        array(
+            'name'  => esc_attr__('Vert', 'themeLangDomain'),
+            'slug'  => 'vert',
+            'color' => '#3B8E46',
+        ),
+    ));
 }
